ef9637868d5a0ad4d7e9a2e37e53d9878d20b16a
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-09-08 01:55:51 +0900

    small change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,12 @@ const Home = () => {
               <span>SUN</span>
             </h2>
           </div>
-          <img src="src/assets/logo-header.png" alt="logo" />
+          <img src="./src/assets/logo-header.png" alt="logo" />
         </div>
         <div className="home-p">
           <h3>荒天の場合は、開催中止となります</h3>
           <h3>in 蓬莱橋897.4広場 10:00〜15:00</h3>
-          <h3>入場無料 ／ 🅿︎専用駐車場 有</h3>
+          <h3>入場無料 / 🅿︎専用駐車場 有</h3>
           <h2>
             日常の中で音を楽しみ、人の温かさを感じる空間を 創造するマルシェ
           </h2>
