a74fe2ce45bbfb84093d97f282f6e8be71b830b3
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-09-28 17:20:14 +0900

    changing p-tag, adding insta img Shop.tsx

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -6,6 +6,7 @@ import s3 from "../assets/s3.jpg";
 import s4 from "../assets/s4.jpeg";
 import s5 from "../assets/s5.jpg";
 import s6 from "../assets/s6.jpg";
+import insta2 from "../assets/insta2.png";
 
 const Shop = () => {
   return (
@@ -18,47 +19,69 @@ const Shop = () => {
           <div className="shop-container">
             <div className="shop-img">
               <img src={s1} alt="" />
-              <p>
-                FEEMUE KLONG TOEY <br />
-                タイ最大のスラム 「クロントイ・スラム」発の
-                ライフスタイルブランド
-              </p>
+              <div className="shops-info">
+                <div className="name">FEEMUE KLONG TOEY</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="shop-img">
               <img src={s2} alt="" />
-              <p>オーガニック抹茶カフェ和み</p>
+              <div className="shops-info">
+                <div className="name">オーガニック抹茶カフェ和み</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="shop-img">
               <img src={s3} alt="" />
-              <p>
-                Terakoyaラフ <br />
-                まなびと学童保育の Terakoyaラフです。
-                科学実験を体験できるワークショップ
-              </p>
+              <div className="shops-info">
+                <div className="name">Terakoyaラフ</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="shop-img">
               <img src={s4} alt="" />
-              <p>
-                87kitchen
-                <br />
-                国産米粉チュロスをぜひ お試し下さい。
-              </p>
+              <div className="shops-info">
+                <div className="name">87kitchen</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="shop-img">
               <img src={s5} alt="" />
-              <p>
-                アゲイン
-                <br />
-                本場宮崎県産鶏の炭火焼！ 炭火の薫りがたまらない。
-                若鶏、親鶏、せせり、ハラミなど 是非ご賞味下さい。
-              </p>
+              <div className="shops-info">
+                <div className="name">アゲイン</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="shop-img">
               <img src={s6} alt="" />
-              <p>
-                MNKY <br />
-                編み小物 デニムリメイクのバッグなど
-              </p>
+              <div className="shops-info">
+                <div className="name">MNKY</div>
+                <div className="shop-insta">
+                  <a href="">
+                    <img src={insta2} alt="" />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
           <div className="more-link">
